3e12a867cd0f0d59c20ccffdd77accb4c1cdb5c6
Eero Savolainen <eeros@debian-BULLSEYE-live-builder-AMD64>
2023-03-13 10:49:56 +0200

    bash

--- T13/rust/rusthello.rs
+++ T13/rust/rusthello.rs
@@ -2,6 +2,6 @@ use std::env;
 
 fn main(){
 	let args: Vec<String> = env::args().collect();
-	println!("I got {:?} usable inputs, they were {:?}", args.len(), &args[0..]);
+	println!("I got {:?} inputs, they were {:?}", args.len(), &args[0..]);
 	println!("Hello {}!", &args[1])
 }
